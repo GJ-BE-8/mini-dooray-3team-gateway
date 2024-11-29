e588cbec11f3902d555bddc148e96c7b249e4179
gimzaezin <gimzaezin@gmail.com>
2024-11-29 18:05:06 +0900

    init

---
+++ src/main/java/com/nhnacademy/minidooray3teamgateway/MiniDooray3teamGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray3teamgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDooray3teamGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDooray3teamGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray3teamgateway/MiniDooray3teamGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray3teamgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDooray3teamGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
